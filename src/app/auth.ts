1b8d9dfd6bd2aa7bcbbfee82b7e062c0be485ecf
sultangithub04 <sult_04@yahoo.com>
2025-10-08 23:37:21 +0600

    credential provider added

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -1,17 +1,76 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
-
+import CredentialsProvider from "next-auth/providers/credentials";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) {
+          console.error("Email or Password is missing");
+          return null
+        }
+
+
+        try {
+          const res = await fetch(
+            `${process.env.NEXT_PUBLIC_BASE_API}/auth/login`,
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                email: credentials.email,
+                password: credentials.password,
+              }),
+            }
+          );
+          // console.log("Response From Backend:", res);
+          if (!res?.ok) {
+            console.error("Login Failed", await res.text());
+            return null;
+          }
+
+          const user = await res.json();
+
+          if (user.id) {
+            return {
+              id: user?.id,
+              name: user?.name,
+              email: user?.email,
+              image: user?.picture,
+              role: user?.role,
+
+            };
+          } else {
+            return null;
+          }
+        } catch (err) {
+          console.error(err);
+          return null;
+        }
+
+      }
+    })
   ],
   secret: process.env.NEXTAUTH_SECRET,
   session: { strategy: "jwt" },
   callbacks: {
-    async jwt({ token, account, profile, user }) {
+    async jwt({ token, account, profile }) {
       if (account && profile) {
         token.email = profile.email;
         token.name = profile.name;
